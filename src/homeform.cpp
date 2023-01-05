56613e12f5fe35b3183e002c936a40e4a67b78cf
Kyle Ross <RossKyle@JohnDeere.com>
2023-01-05 12:56:35 -0600

    build: disabled license timeout, backupTimer & android/linux builds

--- src/homeform.cpp
+++ src/homeform.cpp
@@ -403,7 +403,7 @@ homeform::homeform(QQmlApplicationEngine *engine, bluetooth *bl) {
 #ifndef STEAM_STORE
     connect(engine, &QQmlApplicationEngine::quit, &QGuiApplication::quit);
     connect(&tLicense, &QTimer::timeout, this, &homeform::licenseTimeout);
-    tLicense.start(600000);
+    //tLicense.start(600000);
     licenseRequest();
 #endif
 #endif
@@ -466,7 +466,7 @@ homeform::homeform(QQmlApplicationEngine *engine, bluetooth *bl) {
 
     backupTimer = new QTimer(this);
     connect(backupTimer, &QTimer::timeout, this, &homeform::backup);
-    backupTimer->start(1min);
+    //backupTimer->start(1min);
 
     QObject *rootObject = engine->rootObjects().constFirst();
     QObject *home = rootObject->findChild<QObject *>(QStringLiteral("home"));
